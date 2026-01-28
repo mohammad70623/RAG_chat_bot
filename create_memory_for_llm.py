8722da46468c764c07ba5509b4fe81074cd04e13
mohamad70623 <mohammad70623@gmail.com>
2026-01-28 11:04:51 +0600

    add chunck function for rag project

--- create_memory_for_llm.py
+++ create_memory_for_llm.py
@@ -13,3 +13,12 @@ def load_pdf_files(data):
     
 documents = load_pdf_files(data=DATA_PATH)
 print("Length of pdf", len(documents))
+
+#Step 2: Creat Chunks
+def creat_chunks(extracted_data): 
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, 
+                                                  chunk_overlap = 50)
+    text_chunks = text_splitter.split_documents(extracted_data)
+    return text_chunks 
+text_chunks = creat_chunks(extracted_data = documents)
+print("Length of chunks:", len(text_chunks))
